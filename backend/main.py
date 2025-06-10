d097154924b8a044c3f3a7d049192101c8504bba
CRISTHIAN <ccuevaw1@upao.edu.pe>
2025-06-10 16:35:25 -0500

    agregando tablas de etapa Modelado 5

--- backend/main.py
+++ backend/main.py
@@ -251,7 +251,7 @@ async def proceso_csv_crudo(file: UploadFile = File(...)):
             df_original = pd.read_csv(path_csv, encoding="latin1", sep=";", on_bad_lines='skip')
 
         # Procesar archivo CSV (mineria.py)
-        df_final, resumen, columnas_detectadas, preview_antes, preview_despues = procesar_datos_computrabajo(path_csv)
+        df_final, resumen, columnas_detectadas, preview_antes, preview_despues = procesar_datos_computrabajo(path_csv, df_original)
 
         # Verificar si df_final está vacío (importante validación)
         if df_final.empty:
