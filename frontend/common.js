002116bd707c322f07d1b2ebc14c272be08b75fc
CRISTHIAN <ccuevaw1@upao.edu.pe>
2025-06-20 12:10:45 -0500

    arreglando calculo tiempo skills

--- frontend/common.js
+++ frontend/common.js
@@ -128,7 +128,7 @@ async function subirCSV(file) {
 }
 
 // FUNCIÓN PARA MEDIR Y REGISTRAR TIEMPOS
-async function registrarTiempoCarga(carrera, inicioTiempo) {
+export async function registrarTiempoCarga(carrera, inicioTiempo) {
   const finTiempo = Date.now();
   const tiempoTotal = (finTiempo - inicioTiempo) / 1000;
   
